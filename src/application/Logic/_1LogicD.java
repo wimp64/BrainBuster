b9c8359d35343d5dc640b63dba803744066ebf38
HlanHtetKyaw <133634944+HlanHtetKyaw@users.noreply.github.com>
2023-10-10 22:27:02 +0630

    LGD

--- src/application/Logic/_1LogicD.java
+++ src/application/Logic/_1LogicD.java
@@ -138,6 +138,11 @@ public class _1LogicD extends Application {
 		back.setOnAction(a -> {
 
 			cat.switchToScene1(a, 1, languageChange);
+			timeLine.stop();
+			MathResultCheck.correct=0;
+			MathResultCheck.percent=0;
+			MathResultCheck.result=0;
+			switchingQuestionForDiffLogic.point=-1;;
 		});
 		backArrow.setFitWidth(40);
 		backArrow.setFitHeight(30);
